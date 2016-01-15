f6de6e96eae492d9d3f046c6d1332d3484c8d475
polidog <polidogs@gmail.com>
2016-01-15 18:19:56 +0900

    Update 2016-01-15T09:19:52.850Z

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
 $(function(){
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+
   var recognition = new webkitSpeechRecognition();
   recognition.lang = 'ja';
+
+  var tweet = function(text) {
+    var base = "https://twitter.com/intent/tweet?";
+    var params = {
+      url: "http://tavii.github.io/bals/",
+      text: text,
+      hashtags: "test"
+    };
+
+    var param = $.param(params);
+    $('#tweet-btn').attr('href', base + param);
+  }
+
+  var runFlash = function(){
+    $("#target-img").css("opacity", "0.1");
+    $("#target-img").css("filter", "alpha(opacity=10)");
+    $("#target-img").fadeTo("middle", 1.0);
+  }
+
+
+  $('#bals-btn').on('click',function(){
+    recognition.start();
+  });
 
   // input
   recognition.addEventListener('result', function(event){
@@ -8,10 +34,24 @@ $(function(){
     var input = event.results.item(0).item(0).transcript;
 
     if (a == input) {
-      console.log('barusu')
+      console.log("bal");
+
       $('#target-img').removeClass('hide');
+      $('#tweet-btn').removeClass('hide');
+      tweet('test');
+
+      var target = document.getElementById("tweet-btn");
+    // target.click();
+
+      runFlash();
+
     }
+
   });
 
-  recognition.start();
+  $('#message').textAnimation({
+    speed: 200,
+    delay: 100
+  });
+
 });
